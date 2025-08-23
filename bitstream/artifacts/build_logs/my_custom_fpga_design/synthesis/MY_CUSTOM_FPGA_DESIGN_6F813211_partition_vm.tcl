455a1f23e88baa2ec6dabd3351922aa968741e8f
Ved Jain <vedjain773@gmail.com>
2025-08-24 00:21:14 +0530

    2

---
+++ bitstream/artifacts/build_logs/my_custom_fpga_design/synthesis/MY_CUSTOM_FPGA_DESIGN_6F813211_partition_vm.tcl
@@ -0,0 +1,2 @@
+set_partition_info -name "/MY_CUSTOM_FPGA_DESIGN_6F813211" -timestamp "1755784172"
+set_partition_info -name "/MY_CUSTOM_FPGA_DESIGN_6F813211/BVF_RISCV_SUBSYSTEM_inst_0/MIV_IHC_C0_0/MIV_IHC_C0_0/miv_ihc_core_0" -timestamp "1755784172"
---
+++ bitstream/artifacts/build_logs/my_custom_fpga_design/synthesis/MY_CUSTOM_FPGA_DESIGN_6F813211_syn.tcl
@@ -0,0 +1,3 @@
+project -load "/home/beagle/builds/UqLYJU_q/0/vedj773/count-gateware/builds/my_custom_fpga_design/work/libero/synthesis/MY_CUSTOM_FPGA_DESIGN_6F813211_syn.prj"
+project -run
+project -save
